d6b92c8c7923e7cc6c2d8a2d246ba8898e6244cb
Reekeexxc <122698871+reekee569@users.noreply.github.com>
2023-01-27 01:22:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,7 +7,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-修改默认IP
+
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Change default shell to zsh
